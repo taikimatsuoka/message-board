d6120a199814d5469bd605bd01795e44a930ccc0
taikimatsuoka <mat.su@hotmail.co.jp>
2016-05-15 05:21:10 +0000

    using Bootstrap

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
+
+  
   def index
+    @message = Message.new
+    #messageをすべて表示する
+    @messages = Message.all
   end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path, notics:"メッセージを保存しました"
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:name,:body)
+  end
+
 end
