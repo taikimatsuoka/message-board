df242deb2fc2b20baf37032ac27da65e905ffda8
taikimatsuoka <mat.su@hotmail.co.jp>
2016-05-17 09:56:36 +0000

    valiaty

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ActiveRecord::Base
+    #nameは必須入力かつ二十文字以内
+    validates :name , length: { maximum:20 } , presence: true
+    
+    #内容は必須かつ二文字以上三〇文字以内
+    validates :body , length: { minimum:2 , maximum:30 } , presence: true
 end
